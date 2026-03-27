1bb35de397ac2bc528387306bb5ca9e9f87e2c85
Darcilio Kreitlow <darciliok@gmail.com>
2026-03-27 18:58:53 -0300

    chore: push project updates after verifying build

--- temp-files/debug-topics-structure.sql
+++
@@ -1,6 +0,0 @@
--- Verificar estrutura da tabela topics
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'topics' 
-  AND table_schema = 'public'
-ORDER BY ordinal_position;
