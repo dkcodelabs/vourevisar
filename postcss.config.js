168f2e94dd75521ca0ccf6ddf01e43db480ee26d
Darcilio Kreitlow <darciliok@gmail.com>
2026-04-29 19:27:51 -0300

    fix: resolve PostCSS timeout and CSS @import order issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {
-      overrideBrowserslist: [
-        'last 2 versions',
-        '> 1%',
-        'IE 11',
-        'Chrome >= 60',
-        'Firefox >= 60',
-        'Safari >= 12',
-        'Edge >= 79'
-      ],
-      grid: true,
-      flexbox: 'no-2009'
-    },
+    autoprefixer: {},
   },
 }
+
